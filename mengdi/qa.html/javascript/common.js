2906cb6e94d765b302e4209587680037af76d53a
liubo <283513007@qq.com>
2015-11-08 19:48:56 +0800

    添加修改密码页面，考试页面等

--- mengdi/qa.html/javascript/common.js
+++ mengdi/qa.html/javascript/common.js
@@ -6,7 +6,6 @@ $(function(){
 	$("#left-bar").height(browserHeight)
 	$("#right-container").height(browserHeight)
 	$(".content-list").height(contentHeight)
-	console.log($(".tab-frame").height())
 
 	window.onresize = function(){
 		browserHeight = document.documentElement.clientHeight;
@@ -21,12 +20,26 @@ $(function(){
 	$(".qa-list li").each(function(i){
 		$(this).click(function(){
 			$("#masker").show().height(browserHeight);
-			$("#pop-window").show()
+			$(".pop-qa-detail").show()
+		})
+	})
+
+	$(".ask-btn").click(function(){
+		$("#masker").show().height(browserHeight);
+		$(".pop-qa-ask").show().height(560).css({top:"5%"})
+	})
+
+	$(".ask-tab span").each(function(i){
+		$(this).click(function(){
+			$(".ask-tab span").removeClass("current")
+			$(this).addClass("current")
+			$(".ask-frame").hide()
+			$(".ask-frame:eq("+i+")").show()
 		})
 	})
 
 	$(".close").click(function(){
 		$("#masker").hide()
-		$("#pop-window").hide()
+		$(".pop-window").hide()
 	})
 })
